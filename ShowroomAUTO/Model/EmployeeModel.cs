e70a2205c18eb1045b13326d48d3a0748443cb42
TINO <thongzoke@gmail.com>
2024-11-06 15:32:43 +0700

    add_file_Employee

---
+++ ShowroomAUTO/Model/EmployeeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowroomAUTO.Model
+{
+    internal class EmployeeModel : IModel
+    {
+        public string employeeID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+
+        public bool IsValidate()
+        {
+            if (string.IsNullOrWhiteSpace(employeeID) || string.IsNullOrWhiteSpace(username)
+                || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(role))
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+    }
+}
